b581fbf5fb4320eaca3d3c9cfed14251972c5780
Karel Kadlec <kakadlec@gmail.com>
2021-05-17 21:44:32 -0300

    add headers and new function

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,18 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kakadlec <kakadlec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/17 21:30:28 by kakadlec          #+#    #+#             */
-/*   Updated: 2021/05/17 21:30:29 by kakadlec         ###   ########.fr       */
+/*   Created: 2021/05/17 21:30:31 by kakadlec          #+#    #+#             */
+/*   Updated: 2021/05/17 21:42:58 by kakadlec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
+void	*memcpy(void *dest, const void *src, size_t n)
 {
-	ft_memset(s, 0, n);
+	size_t i;
+
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = (unsigned char)src[i];
+		i++;
+	}
+	return (dest);
 }
