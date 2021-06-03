24325a14e2df3a22e1426484891cf8723166e6f3
Karel Kadlec <kakadlec@gmail.com>
2021-06-03 20:45:01 -0300

    order by name

--- Makefile
+++ Makefile
@@ -6,46 +6,45 @@
 #    By: kakadlec <kakadlec@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/03 13:50:38 by kakadlec          #+#    #+#              #
-#    Updated: 2021/06/03 18:47:42 by kakadlec         ###   ########.fr        #
+#    Updated: 2021/06/03 20:25:36 by kakadlec         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-SRCS	=	ft_memset.c		\
+SRCS	=	ft_atoi.c		\
 			ft_bzero.c		\
-			ft_memcpy.c		\
+			ft_calloc.c		\
+			ft_isalnum.c	\
+			ft_isalpha.c	\
+			ft_isascii.c	\
+			ft_isdigit.c	\
+			ft_isprint.c	\
+			ft_itoa.c		\
 			ft_memccpy.c	\
-			ft_memmove.c	\
 			ft_memchr.c		\
 			ft_memcmp.c		\
-			ft_strlen.c		\
-			ft_strlcpy.c	\
-			ft_strlcat.c	\
-			ft_strchr.c		\
-			ft_strrchr.c	\
-			ft_strnstr.c	\
-			ft_strncmp.c	\
-			ft_atoi.c		\
-			ft_isalpha.c	\
-			ft_isdigit.c	\
-			ft_isalnum.c	\
-			ft_isascii.c	\
-			ft_isprint.c	\
-			ft_toupper.c	\
-			ft_tolower.c	\
-			ft_calloc.c		\
-			ft_strdup.c		\
-			ft_substr.c		\
-			ft_strjoin.c	\
-			ft_strtrim.c	\
-			ft_split.c		\
-			ft_itoa.c		\
-			ft_strmapi.c	\
-			ft_strnew.c		\
+			ft_memcpy.c		\
+			ft_memmove.c	\
+			ft_memset.c		\
 			ft_putchar_fd.c	\
-			ft_putstr_fd.c	\
 			ft_putendl_fd.c	\
 			ft_putnbr_fd.c	\
-
+			ft_putstr_fd.c	\
+			ft_split.c		\
+			ft_strchr.c		\
+			ft_strdup.c		\
+			ft_strjoin.c	\
+			ft_strlcat.c	\
+			ft_strlcpy.c	\
+			ft_strlen.c		\
+			ft_strmapi.c	\
+			ft_strncmp.c	\
+			ft_strnew.c		\
+			ft_strnstr.c	\
+			ft_strrchr.c	\
+			ft_strtrim.c	\
+			ft_substr.c		\
+			ft_tolower.c	\
+			ft_toupper.c	\
 
 OBJS		= $(SRCS:.c=.o)
 
@@ -57,7 +56,11 @@ NAME		= libft.a
 
 all:		$(NAME)
 $(NAME):	$(OBJS)
-				ar rcs $(NAME) $(OBJS)
+				@ar -rcs $(NAME) $(OBJS)
+				@ranlib $(NAME)
+
+.c.o:
+			@clang -Wall -Wextra -Werror -c $< -o ${<:.c=.o} -I./
 clean:
 				$(RM) $(OBJS)
 fclean:		clean
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kakadlec <kakadlec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 21:30:37 by kakadlec          #+#    #+#             */
-/*   Updated: 2021/05/31 20:52:17 by kakadlec         ###   ########.fr       */
+/*   Updated: 2021/06/03 20:12:43 by kakadlec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,44 +16,43 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-char	*ft_strnew(size_t size);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-char	*ft_strnstr(const char	*big, const char *little, size_t len);
-char	*ft_strdup(const char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
-char	*ft_itoa(int n);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strnew(size_t size);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
+int		ft_atoi(const char *nptr);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_atoi(const char *nptr);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_toupper(int c);
 
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
 
+void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-
-void	*ft_memset(void *s, int c, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_calloc(size_t nmemb, size_t size);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
