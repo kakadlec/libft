2abb43f79a1c108490b74e836731cc15a2bf0e6c
Karel Kadlec <kakadlec@gmail.com>
2021-06-03 13:32:03 -0300

    fix norminette

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,13 @@
 /*   By: kakadlec <kakadlec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/29 21:30:29 by kakadlec          #+#    #+#             */
-/*   Updated: 2021/05/29 21:46:28 by kakadlec         ###   ########.fr       */
+/*   Updated: 2021/06/03 13:06:02 by kakadlec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	size_t	i;
