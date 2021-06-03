702f7e3bfafcccaaf60b2d498eb989aa0ee2396f
Karel Kadlec <kakadlec@gmail.com>
2021-06-03 19:17:04 -0300

    fix bullshit test

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: kakadlec <kakadlec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/29 21:30:29 by kakadlec          #+#    #+#             */
-/*   Updated: 2021/06/03 14:21:34 by kakadlec         ###   ########.fr       */
+/*   Updated: 2021/06/03 19:14:56 by kakadlec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 
 	i = 0;
+	if (!s)
+		return (0);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
 	str = malloc((len + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
