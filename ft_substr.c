8195a60065a10d6f07fbfe6ea018a816ce012b90
Karel Kadlec <kakadlec@gmail.com>
2021-06-05 16:54:45 -0300

    test function on guaca

--- ft_substr.c
+++ ft_substr.c
@@ -6,31 +6,50 @@
 /*   By: kakadlec <kakadlec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/29 21:30:29 by kakadlec          #+#    #+#             */
-/*   Updated: 2021/06/03 19:14:56 by kakadlec         ###   ########.fr       */
+/*   Updated: 2021/06/05 16:54:23 by kakadlec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static char	*get_newstr(unsigned int start, size_t size_s, size_t len)
+{
+	char	*new_str;
+	size_t	last_index;
+
+	if (start > size_s)
+		last_index = 0;
+	else if (size_s - start < len)
+		last_index = size_s - start;
+	else
+		last_index = len;
+	new_str = ft_calloc(last_index + 1, sizeof(char));
+	if (new_str)
+		new_str[last_index] = 0;
+	return (new_str);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*str;
-	size_t	i;
+size_t	count;
+	size_t	size_s;
+	char	*new_str;
 
-	i = 0;
 	if (!s)
-		return (0);
-	if (start > ft_strlen(s))
-		return (ft_strdup(""));
-	str = malloc((len + 1) * sizeof(char));
-	if (!str)
 		return (NULL);
-	while (*(s + start) && i < len)
+	size_s = ft_strlen(s);
+	new_str = get_newstr(start, size_s, len);
+	if (new_str)
 	{
-		*(str + i) = *(s + start);
-		start++;
-		i++;
+		count = 0;
+		if (start <= ft_strlen(s))
+		{
+			while (count < len && s[start + count])
+			{
+				new_str[count] = s[start + count];
+				count++;
+			}
+		}
 	}
-	*(str + i) = '\0';
-	return (str);
+	return (new_str);
 }
