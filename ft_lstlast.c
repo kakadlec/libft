36828e311837c953b4cdfbad2e9b14485a7ad326
Karel Kadlec <kakadlec@gmail.com>
2021-06-05 16:50:36 -0300

    fix norminette

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: kakadlec <kakadlec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 17:22:53 by kakadlec          #+#    #+#             */
-/*   Updated: 2021/06/05 16:40:25 by kakadlec         ###   ########.fr       */
+/*   Updated: 2021/06/05 16:50:16 by kakadlec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	if(lst)
+	if (lst)
 	{
 		while (lst->next != NULL)
-		lst = lst->next;
+			lst = lst->next;
 	}
 	return (lst);
 }
